326392a08d8bcf116ab440c9c74490bde782f818
Jack Edge <js77js@yahoo.co.uk>
2024-06-11 19:26:07 +0100

    adding better comments

--- pkg/shapes/shapes.go
+++ pkg/shapes/shapes.go
@@ -8,23 +8,26 @@ type shape interface {
 	printArea()
 }
 
+// triangle and square structs
 type triangle struct {
 	base   int
 	height int
-}
-
-func (t triangle) printArea() {
-	fmt.Printf("area of %T is %.2f\n", t, (0.5 * float64(t.base) * float64(t.height)))
 }
 
 type square struct {
 	side int
 }
 
+// attach printArea() methods to both triangle and square promoting them both to a shape interface
+func (t triangle) printArea() {
+	fmt.Printf("area of %T is %.2f\n", t, (0.5 * float64(t.base) * float64(t.height)))
+}
+
 func (s square) printArea() {
 	fmt.Printf("area of %T is %.2f\n", s, float64(s.side*s.side))
 }
 
+// function that take a shape interface (ie: can accept a promoted square or a promoted triangle)
 func printArea(sh shape) {
 	sh.printArea()
 }
